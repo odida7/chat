e0656e4ab49f1dc92f5d35ca7f5812e1032dfa5f
odida7 <98588349+odida7@users.noreply.github.com>
2023-01-29 16:16:54 +0300

    Add files via upload

---
+++ client/src/Chat.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+function Chat({socket, username, room}) {
+
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [messageList, setMessageList] = useState([]); 
+
+  const sendMessage = async() => {
+    if (currentMessage !== '') {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time: new Date(Date.now()).getHours() + ':' + 
+        new Date(Date.now()).getMinutes()
+      };
+
+      await socket.emit('send_message', messageData);
+      setMessageList((list) => [...list, messageData]);
+
+    }
+  };
+ 
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className='chat-window'>
+       <div className='chat-header'>
+         <p>live chat</p>
+       </div>
+
+       <div className='chat-body'>
+         <ScrollToBottom className='message-container'>
+         {messageList.map((messageContent) =>{
+           return (
+              <div className='message'
+               id={username === messageContent.author ? 'you' : 'other'} >
+                <div>
+                  <div className='message-content'>
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className='messgae-meta'>
+                    <p id='time'>{messageContent.time}</p>
+                    <p id='author'>{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+           )
+         })}
+         </ScrollToBottom>
+       </div>
+
+       <div className='chat-footer'>
+         <input type='text'
+          placeholder='text' 
+          onChange={(event) => {
+        setCurrentMessage(event.target.value);
+          }}
+        onKeyPress={(event) => {
+          event.key === 'Enter' && sendMessage();
+        
+      }}/>
+         <button onClick={sendMessage}>send</button>
+       </div>
+    </div>
+  )
+ }
+
+export default Chat;
